70ac4adad1fca8e1f602c327eaab6672b6a699b7
Adam White <adjackwhite@gmail.com>
2020-12-11 16:11:32 +0800

    updated filters & search

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,18 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update]
 
-
   def index
     if current_user.manager?
       @freelancers = current_user.profiles
+      @freelancers = @freelancers.profile_search(params[:query]) if params[:query].present?
+      @freelancers = @freelancers.profession_search(params[:profession]) if params[:profession].present?
+      @freelancers = @freelancers.skill_search(params[:skill]) if params[:skill].present?
+      @freelancers = @freelancers.location_search(params[:location]) if params[:location].present?
+      @freelancers = @freelancers.where('lowest_day_rate >= ?', params[:lowest_rate]) if params[:lowest_rate].present?
+      @freelancers = @freelancers.where('highest_day_rate <= ?', params[:highest_rate]) if params[:highest_rate].present?
+
     else
-      @managers = current_user.managers
+      @managers = current_user.managerss
     end
   end
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,36 @@
 class Profile < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :website_links, dependent: :destroy
   has_many :profile_skills, dependent: :destroy
   has_many :skills, through: :profile_skills
   has_many :networks, dependent: :destroy
   has_one_attached :photo
+  pg_search_scope :profile_search,
+    against: [ :profession, :location ],
+    associated_against: {
+      user: [ :first_name, :last_name, :email, :whatsapp_number ],
+      skills: [ :skill ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+  pg_search_scope :profession_search,
+    against: [ :profession ],
+    using: {
+      tsearch: { prefix: true }
+    }
+  pg_search_scope :skill_search,
+    associated_against: {
+      skills: [ :skill ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+  pg_search_scope :location_search,
+    against: [ :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
