fa99d0205fa718a65419ad120c0d592bef4026cb
Adam White <adjackwhite@gmail.com>
2020-12-12 17:00:12 +0800

    added select2 to filters

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    placeholder: "Filter",
+    allowClear: true,
+  });
 };
 
 export { initSelect2 };
