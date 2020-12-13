6e991e58a2a98d5681e67e9b1bec60437a602886
Adam White <adjackwhite@gmail.com>
2020-12-13 18:15:47 +0800

    update

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,29 @@
+class NotesController < ApplicationController
+before_action :set_profile, except: :destroy
+
+  def create
+    @note = Note.new(note_params)
+    @note.profile = @profile
+    @note.save
+    redirect_to profile_path(@profile)
+  end
+
+  def update
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to profile_path(params[:profile_id])
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content)
+  end
+
+  def set_profile
+    @profile = Profile.find(params[:profile_id])
+  end
+end
