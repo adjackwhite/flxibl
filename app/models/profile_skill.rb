236bf577f05b4f8e1f9d4b46ffdf324340e398b6
Adam White <adjackwhite@gmail.com>
2020-12-14 10:20:03 +0800

    updated skills for

--- app/models/profile_skill.rb
+++ app/models/profile_skill.rb
@@ -1,4 +1,13 @@
 class ProfileSkill < ApplicationRecord
   belongs_to :profile
   belongs_to :skill
+
+
+  validate :max_skills
+
+  def max_skills
+    if self.profile.skills.count >= 5
+      errors.add(:profile,  "Only add 5 skills")
+    end
+  end
 end
